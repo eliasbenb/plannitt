313242eae0a5d21ebfae0cea954beb763eddba80
Elias Benbourenane <eliasbenbourenane@gmail.com>
2021-11-10 10:47:20 +0400

    Completed timetable

--- client/src/pages/User/index.js
+++ client/src/pages/User/index.js
@@ -48,204 +48,49 @@ export class CalendarUser {
   }
 }
 
-export class TimetableNode {
-  constructor(data) {
-    this.data = data;
-    this.day = data.day;
-    this.hour = data.hour;
-    this.time = data.time;
-    this.unix_time = data.unix_time;
-    this.next = null;
-  }
-}
-
 export class TimetableUser {
   constructor(data, mode, update) {
     this._id = data._id;
     this.data = data;
-    this.head = null;
     this.mode = mode;
     this.name = data.name;
     this.times = this.data.times;
     this.update = update;
-    this.size = null;
     this.init();
   }
 
   init() {
     this.sort();
-    this.size = this.times.length;
-    if (this.times.length) {
-      this.head = new TimetableNode(this.times[0]);
-      var node = this.head;
-      for (let i = 1; i < this.times.length; i++) {
-        node.next = new TimetableNode(this.times[i]);
-        node = node.next;
-      }
-    }
     this.update();
   }
 
-  last() {
-    var node = this.head;
-    while (node) {
-      if (node.next) {
-        node = node.next;
+  add(value) {
+    this.times.push({
+      day: value.day,
+      hour: value.hour,
+      unix_time: value.unix_time,
+    });
+    this.update();
+  }
+
+  remove(unix_time) {
+    for (let i = 0; i < this.times.length; i++) {
+      if (this.times[i].unix_time == unix_time) {
+        this.times.splice(i, 1);
+        this.update();
+        return true;
       }
     }
-    return node;
+    return false;
   }
 
-  find(value) {
-    var node = this.head;
-    var count = 0;
-    while (node) {
-      if (node.unix_time == value) {
-        return count;
-      } else {
-        node = node.next;
+  find(unix_time) {
+    for (let i = 0; i < this.times.length; i++) {
+      if (this.times[i].unix_time == unix_time) {
+        return this.times[i];
       }
-      count++;
     }
-    return null;
-  }
-
-  unixify(value) {
-    const day_list = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-    var day_index = day_list.indexOf(value.day, 0);
-    var unix_time = (day_index * 24 + value.hour) * 3600;
-    return unix_time;
-  }
-
-  add(element) {
-    // creates a new node
-    var node = new TimetableNode(element);
-    // to store current node
-    var current;
-
-    // if list is Empty add the
-    // element and make it head
-    if (this.head == null) this.head = node;
-    else {
-      current = this.head;
-
-      // iterate to the end of the
-      // list
-      while (current.next) {
-        current = current.next;
-      }
-
-      // add node
-      current.next = node;
-    }
-    this.size++;
-  }
-
-  insertAt(element, index) {
-    if (index < 0 || index > this.size)
-      return console.log("Please enter a valid index.");
-    else {
-      // creates a new node
-      var node = new TimetableNode(element);
-      var curr, prev;
-
-      curr = this.head;
-
-      // add the element to the
-      // first index
-      if (index == 0) {
-        node.next = this.head;
-        this.head = node;
-      } else {
-        curr = this.head;
-        var it = 0;
-
-        // iterate over the list to find
-        // the position to insert
-        while (it < index) {
-          it++;
-          prev = curr;
-          curr = curr.next;
-        }
-
-        // adding an element
-        node.next = curr;
-        prev.next = node;
-      }
-      this.size++;
-    }
-  }
-
-  removeFrom(index) {
-    if (index < 0 || index >= this.size)
-      return console.log("Please Enter a valid index");
-    else {
-      var curr,
-        prev,
-        it = 0;
-      curr = this.head;
-      prev = curr;
-
-      // deleting first element
-      if (index === 0) {
-        this.head = curr.next;
-      } else {
-        // iterate over the list to the
-        // position to removce an element
-        while (it < index) {
-          it++;
-          prev = curr;
-          curr = curr.next;
-        }
-
-        // remove the element
-        prev.next = curr.next;
-      }
-      this.size--;
-
-      // return the remove element
-      return curr.element;
-    }
-  }
-
-  removeElement(element) {
-    var current = this.head;
-    var prev = null;
-
-    // iterate over the list
-    while (current != null) {
-      // comparing element with current
-      // element if found then remove the
-      // and return true
-      if (current.element === element) {
-        if (prev == null) {
-          this.head = current.next;
-        } else {
-          prev.next = current.next;
-        }
-        this.size--;
-        return current.element;
-      }
-      prev = current;
-      current = current.next;
-    }
-    return -1;
-  }
-
-  isEmpty() {
-    return this.size == 0;
-  }
-
-  size_of_list() {
-    console.log(this.size);
+    return false;
   }
 
   sort() {
@@ -283,8 +128,9 @@ export default class UserPage extends Component {
 
     this.update = this.update.bind(this);
     this.onDaysChange = this.onDaysChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-    this.onSelectDay = this.onSelectDay.bind(this);
+    this.unixify = this.unixify.bind(this);
+    this.onSubmitCalendar = this.onSubmitCalendar.bind(this);
+    this.onSubmitTimetable = this.onSubmitTimetable.bind(this);
   }
 
   componentDidMount() {
@@ -329,7 +175,7 @@ export default class UserPage extends Component {
     this.setState({ days: evt });
   }
 
-  onSelectDay(day_number, hour, time) {
+  unixify(value) {
     const day_list = [
       "Sunday",
       "Monday",
@@ -339,15 +185,44 @@ export default class UserPage extends Component {
       "Friday",
       "Saturday",
     ];
-    let day = day_list[day_number];
-    let data = { day: day, hour: hour, time: time };
-    this.setState({ days: [...this.state.days, data] });
+    var day_index = day_list.indexOf(value.day, 0);
+    var unix_time = (day_index * 24 + value.hour) * 3600;
+    return unix_time;
   }
 
-  onSubmit() {
+  onSubmitCalendar() {
     let { days, name, oid, password } = this.state;
 
     this.state.user.postTime(days);
+
+    let req_path = `/api/v1/planner/post/${oid}/${name}`;
+    let req_args = `?password=${encodeURIComponent(password || "")}`;
+
+    axios
+      .post(req_path + req_args, this.state.user.times)
+      .then((response) => {
+        let data = response.data;
+        if (data.success) {
+          alert("Data submitted successfully.");
+          this.setState({ data: data.content });
+        } else {
+          window.alert(data.message || "Error!");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        if (error.response) {
+          window.alert(error.response.message || "Error!");
+        } else {
+          window.alert("Error!");
+        }
+      });
+  }
+
+  onSubmitTimetable() {
+    let { name, oid, password } = this.state;
+
+    this.state.user.sort();
 
     let req_path = `/api/v1/planner/post/${oid}/${name}`;
     let req_args = `?password=${encodeURIComponent(password || "")}`;
@@ -400,7 +275,7 @@ export default class UserPage extends Component {
             {user.name}
           </Typography>
           <Calendar
-            value={user.times}
+            value={days}
             onChange={this.onDaysChange}
             colorPrimary={theme.palette.primary.main}
             slideAnimationDuration="0.2s"
@@ -416,7 +291,7 @@ export default class UserPage extends Component {
           <Button
             variant="contained"
             color="primary"
-            onClick={this.onSubmit}
+            onClick={this.onSubmitCalendar}
             startIcon={<BackupIcon />}
             style={{
               width: "150px",
@@ -493,11 +368,11 @@ export default class UserPage extends Component {
                       "Friday",
                       "Saturday",
                     ].map((a, b) =>
-                      user.find(
-                        user.unixify({ day: a, hour: n + 6, time: t })
-                      ) ? (
+                      user.find(this.unixify({ day: a, hour: n + 6 })) ? (
                         <td
-                          onClick={() => user.onSelectDay(b, n + 6, t)}
+                          onClick={() =>
+                            user.remove(this.unixify({ day: a, hour: n + 6 }))
+                          }
                           key={a}
                         >
                           ✔️
@@ -505,7 +380,11 @@ export default class UserPage extends Component {
                       ) : (
                         <td
                           onClick={() =>
-                            user.add({ day: a, hour: n + 6, time: t })
+                            user.add({
+                              day: a,
+                              hour: n + 6,
+                              unix_time: this.unixify({ day: a, hour: n + 6 }),
+                            })
                           }
                           key={a}
                         ></td>
@@ -516,6 +395,28 @@ export default class UserPage extends Component {
               </tbody>
             </table>
           </div>
+          <MuiAlert
+            elevation={6}
+            variant="filled"
+            severity="info"
+            style={{ marginTop: "30px" }}
+          >
+            Select the dates when you are available.
+          </MuiAlert>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={this.onSubmitTimetable}
+            startIcon={<BackupIcon />}
+            style={{
+              width: "150px",
+              height: "50px",
+              alignSelf: "center",
+              marginTop: "25px",
+            }}
+          >
+            Submit
+          </Button>
         </div>
       </div>
     ) : (
